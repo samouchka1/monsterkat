da15ecf644f2d8f29954f8704b245f80c552ca0f
Miguel C <82068124+samouchka1@users.noreply.github.com>
2024-02-18 16:05:16 -0500

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,16 +4,25 @@ const Content = () => {
   return (
     <article>
         <section className="relative max-w-screen-xl mx-auto mt-[100px] pt-10">
-            <div className="flex w-[500px] justify-end items-center">
-                <div className="rotate-[-90deg] text-neutral-50 h-6 flex-none">
-                    <span className="text-monsterkat-green">Instinct</span> — Released May 22, 2020
+            <div className="w-full grid grid-cols-[24px,480px,1fr] gap-1">
+                <div>
+                    <div className="absolute bottom-0 rotate-[180deg] text-neutral-50" style={{writingMode: 'vertical-rl'}}>
+                        <span className="text-monsterkat-green">Instinct</span> — Released May 22, 2020
+                    </div>
                 </div>
-                <div className="flex-grow">
-                    <img src="/album_cover.png" alt="album cover" />
+                <div>
+                    <img src="/album_cover.png" alt="album cover" width="480" />
                 </div>
-            </div>
-            <div className="flex">
-
+                <div className="flex flex-col pl-16">
+                    <div>
+                        <h1 className="text-neutral-50 text-6xl">Level Days</h1>
+                        <p className="text-neutral-50 text-4xl">conro</p>
+                    </div>
+                    <div>
+                        <button className="text-neutral-50 text-2xl bg-monsterkat-green py-2 w-[212px] align-middle">listen now</button>
+                        <a href="#" title="share">Share</a>
+                    </div>
+                </div>
             </div>
         </section>
 
